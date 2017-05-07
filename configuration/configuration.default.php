bbc7fad1c930058dc783a4f1fd92f93e510e6e59
rjcorflo <rj.corchero@gmail.com>
2017-05-07 23:52:46 +0200

    [DEV] Add default configuration file

---
+++ configuration/configuration.default.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This file is the template with all parameters needed for correct use of Application.
+ *
+ * This should be renamed as configuration.php and filled with correct parameters.
+ */
+
+return [
+    'app.baseDir' => '',
+    'database.dsn' => '',
+    'database.user' => '',
+    'database.pass' => ''
+];
