783681d41b2c9c01338d892a394ecfd79ca44388
rjcorflo <rj.corchero@gmail.com>
2017-05-17 00:48:01 +0200

    [DEV] Add configuration default

--- configuration/configuration.default.php
+++ configuration/configuration.default.php
@@ -1,6 +1,9 @@
 <?php
-
-use function DI\string;
+/**
+ * This file is the template with all parameters needed for correct use of Application.
+ *
+ * This should be renamed as configuration.php and filled with correct parameters.
+ */
 
 return [
     'database.dsn' => string('sqlite:{app.storageDir}/test.db'),
