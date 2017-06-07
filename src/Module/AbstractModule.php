401879a2250f0bbab1bc71a2868932b87ea5526f
rjcorflo <rj.corchero@gmail.com>
2017-06-07 22:30:16 +0200

    [DEV] Add comment

--- src/Module/AbstractModule.php
+++ src/Module/AbstractModule.php
@@ -4,6 +4,13 @@ namespace RJ\PronosticApp\Module;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+/**
+ * Class AbstractModule
+ *
+ * Base class for all modules.
+ *
+ * @package RJ\PronosticApp\Module
+ */
 abstract class AbstractModule implements EventSubscriberInterface
 {
 }
