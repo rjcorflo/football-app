518cf40fbc5cb6e9bda6b4a00d4592d4c10f29a4
rjcorflo <rj.corchero@gmail.com>
2017-07-20 23:02:10 +0200

    [DEV] Project with Symfony Flex

--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    'Symfony\Bundle\WebServerBundle\WebServerBundle' => ['dev' => true],
     'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
     'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
     'Symfony\Bundle\SecurityBundle\SecurityBundle' => ['all' => true],
@@ -9,4 +8,6 @@ return [
     'Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle' => ['all' => true],
     'Doctrine\Bundle\DoctrineBundle\DoctrineBundle' => ['all' => true],
     'JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle' => ['all' => true],
+    'Symfony\Bundle\WebServerBundle\WebServerBundle' => ['dev' => true],
+    'Symfony\Bundle\WebProfilerBundle\WebProfilerBundle' => ['dev' => true, 'test' => true],
 ];
